88b43313bd0889c79e5686ba3a618d76a9299a83
Aniekan Offiong <ekanoffiong@gmail.com>
2017-11-05 23:50:55 +0100

    Removed additional elements

--- server/config/main.js
+++ server/config/main.js
@@ -2,5 +2,5 @@
 module.exports = {  
     'database': 'mongodb://127.0.0.1:27017/alc-app',
     'port': process.env.PORT || 3000,
-    'hostname' : 'http://localhost:3000'
+    'hostname' : 'https://andela-alc.herokuapp.com'
 };
